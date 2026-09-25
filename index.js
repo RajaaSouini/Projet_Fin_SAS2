9d417df4ea47546ffc356537b3c7fc32c9b99276
RajaaSouini <rajaasouini@gmail.com>
2026-09-25 09:40:19 +0000

    step1

---
+++ index.js
@@ -0,0 +1,39 @@
+//Step 1 
+
+const candidats = [{
+    cin: "AB123456",
+	nom: "Boushaba",
+	prenom: "Soufiane",
+	partiPolitique: "Indépendant",
+	age: 40,
+	electeurs: []
+}];
+//step 2
+
+candidats.push({
+	cin: "HH312673",
+	nom: "Souini",
+	prenom: "Rajaa",
+	partiPolitique: "Indépendant",
+	age: 22,
+	electeurs: []
+},
+{
+	cin: "KO786432",
+	nom: "Rhazoui",
+	prenom: "Fahd",
+	partiPolitique: "Indépendant",
+	age: 24,
+	electeurs: []
+},
+{
+	cin: "DH7632",
+	nom: "Alami",
+	prenom: "Hiba",
+	partiPolitique: "Indépendant",
+	age: 20,
+	electeurs: []
+});
+
+console.log(candidats);
+
